6c03cf10c782a9ba6397368ab0a3699e306a0836
Jeinic07 <uyabanjeisson04@gmail.com>
2023-08-28 12:09:20 -0500

    Se crearon las funciones basicas del crud (solo sirve una)

--- src/co/edu/unbosque/controller/Controller.java
+++ src/co/edu/unbosque/controller/Controller.java
@@ -1,5 +1,89 @@
 package co.edu.unbosque.controller;
 
+import java.util.Scanner;
+
+import co.edu.unbosque.model.VehiculoDAO;
+
 public class Controller {
 
+	private VehiculoDAO v;
+	private Scanner sc;
+
+	public Controller() {
+		v = new VehiculoDAO();
+		sc = new Scanner(System.in);
+	}
+	
+	public void run() {
+		
+		ciclo1: while(true) {
+			
+			System.out.println("__________________________________________");
+			System.out.println("");
+			System.out.println("SISTEMA DE ALMACENAMIENTO DE VEHICULOS");
+			System.out.println("");
+			System.out.println("Que desea realizar?");
+			System.out.println("");
+			System.out.println("1. Ingresar un vehiculo.");
+			System.out.println("2. Eliminar un vehiculo.");
+			System.out.println("3. Mostrar un vehiculo.");
+			System.out.println("4. Mostrar todos los vehiculos.");
+			System.out.println("5. Salir.");
+			System.out.println("");
+			System.out.println("__________________________________________");
+			
+			
+			ciclo2: while(true) {
+				
+				int op = sc.nextInt();
+				
+				switch(op) {
+				
+				case 1:{
+					
+					System.out.println("Ingrese el tipo de vehiculo (camioneta, automovil, moto");
+					String tipo = sc.nextLine();
+					sc.next();
+					
+					System.out.println("Ingrese la placa del vehiculo (AAA 000)");
+					String placa = sc.nextLine();
+					sc.next();
+					System.out.println("Ingrese la hora de ingreso (formato 24 horas -> 15 00)");
+					String horaEntrada = sc.nextLine();
+					sc.next();
+					
+					System.out.println(v.ingresar(tipo, placa, horaEntrada));
+					break ciclo2;
+				}
+				case 2:{
+					
+					break ciclo2;
+					
+				}		
+				
+				case 3:{
+					System.out.println("Ingrese la placa del vehiculo que desea ver (AAA 000)");
+					String placa = sc.nextLine();
+					sc.next();
+					
+					System.out.println(v.mostrarVehiculo(placa).toString());
+				
+					break ciclo2;
+				}
+				
+				case 4:{
+					
+					break ciclo2;
+				}
+				
+				case 5:{
+					
+					System.exit(0);
+				}
+				}
+				
+			}
+			
+		}
+	}
 }
